f24f26876769e29c8dcde275c351c548d4a2d815
shalinibalodi <shalini.balodi_ug23@ashoka.edu.in>
2023-12-12 04:36:04 +0530

    updated spend, MintToken and TransactionList

--- frontend/src/components/TransactionList.js
+++ frontend/src/components/TransactionList.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 const TransactionList = () => {
-  const [transactions, setTransactions] = useState([]);
-
+  const [TXID, setTXID] = useState([]);
   useEffect(() => {
-    // Fetch transactions from your Node.js backend
-    axios.get('http://localhost:3001/transactions')
-      .then(response => setTransactions(response.data))
-      .catch(error => console.error('Error fetching transactions:', error));
+    // Fetch toppings data from the backend API
+    const fetchTXID = async () => {
+      try {
+        const response = await fetch('YOUR_BACKEND_API_ENDPOINT');
+        const data = await response.json();
+        setTXID(data); // Update the state with fetched toppings
+      } catch (error) {
+        console.error('Error fetching toppings:', error);
+      }
+    };
+
+    fetchTXID();
   }, []);
+
 
   return (
     <div>
       <h2>Transaction List</h2>
-      {transactions.length === 0 ? (
+      {TXID.length === 0 ? (
         <p>No UTXO transactions available.</p>
       ) : (
         <ul>
-          {transactions.map((transaction) => (
+          {TXID.map((transaction) => (
             <li key={transaction.id}>{transaction.description}</li>
           ))}
         </ul>
@@ -29,22 +37,9 @@ const TransactionList = () => {
 
 export default TransactionList;
 
-//import React, { useEffect, useState } from 'react';
-
-//const TransactionList = () => {
-//  const [transactions, setTransactions] = useState([]);
-
-//  useEffect(() => {
-    // Implement logic to fetch transactions from your smart contract
-//    console.log('Fetching transactions');
-//  }, []);
-
-//  return (
-//    <div>
-//      <h2>Transaction List</h2>
-//      {/* Display the list of transactions */}
-//    </div>
-//  );
-//};
-
-//export default TransactionList;
+//useEffect(() => {
+  // Fetch transactions from your Node.js backend
+//  axios.get('http://localhost:3001/transactions')
+//    .then(response => setTransactions(response.data))
+//    .catch(error => console.error('Error fetching transactions:', error));
+//}, []);
--- frontend/src/components/spend.js
+++ frontend/src/components/spend.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import './style.css';
+//import { toppings } from "./utils/toppings";
 //import { initiateTransaction } from '../services/FabcoinService';
 
+
+
 const FabcoinTransaction = () => {
-  const [amount, setAmount] = useState(false);
+
+  useEffect(() => {
+    // Fetch toppings data from the backend API
+    const fetchTXID = async () => {
+      try {
+        const response = await fetch('YOUR_BACKEND_API_ENDPOINT');
+        const data = await response.json();
+        setTXID(data); // Update the state with fetched toppings
+        setCheckedState(new Array(data.length).fill(false)); // Initialize checkedState based on the length of toppings
+      } catch (error) {
+        console.error('Error fetching toppings:', error);
+      }
+    };
+
+    fetchTXID();
+  }, []); // Empty dependency array to run the effect only once on component mount
+
+  const [checkedState, setCheckedState] = useState([]);
   const [receiver, setReceiver] = useState("");
 
   // State to store the list of input pairs
   const [inputList, setInputList] = useState([])
-
+  const [total, setTotal] = useState(0);
+  const [TXID, setTXID] = useState([]);
   // States for checking the errors
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
 
   // Handling the name change
-  const handleAmount = (e) => {
-    const value =
-    e.target.type === "checkbox" ? e.target.checked : e.target.value;
-    setAmount(value);
-      //setAmount(e.target.value);
+  const handleAmount = (position) => {
+    //const value =
+    //e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    //setAmount(value);
+    const updatedCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + TXID[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setTotal(totalPrice);
     setSubmitted(false);
   };
 
@@ -46,6 +83,8 @@ const FabcoinTransaction = () => {
     }
   };
 
+  const getFormattedPrice = (price) => `$${price.toFixed(2)}`; 
+
 // Showing success message
 const successMessage = () => {
     return (
@@ -55,7 +94,7 @@ const successMessage = () => {
                 display: submitted ? "" : "none",
             }}
         >
-            <h1>Transferred {amount ? 'some' : 'no'} coins to {receiver} successfully!!</h1>
+            <h1>Transferred {total ? 'some' : 'no'} coins to {receiver} successfully!!</h1>
         </div>
     );
 };
@@ -77,12 +116,12 @@ const errorMessage = () => {
   const handleTransaction = (e) => {
     // Implement transaction initiation logic
     e.preventDefault();
-    if (amount === 0 || receiver === "") {
+    if (total === 0 || receiver === "") {
         setError(true);
     } else {
         setSubmitted(true);
         setError(false);
-        console.log('Sending Transaction:', { amount, receiver });
+        console.log('Sending Transaction:', { total, receiver });
         setInputList([]);
     }
     //console.log('Sending Transaction:', { amount, receiver });
@@ -93,8 +132,35 @@ const errorMessage = () => {
       <h2>Fabcoin Transfer</h2>
       <form>
         <label>
-          UTXO:
-          <input type="checkbox" name="hooks" checked={amount} onChange={handleAmount} />
+          <h3>Select UTXOs:</h3>
+          <ul className="toppings-list">
+        {TXID.map(({ name, price }, index) => {
+          return (
+            <li key={index}>
+              <div className="toppings-list-item">
+                <div className="left-section">
+                  <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={name}
+                    value={name}
+                    checked={checkedState[index]}
+                    onChange={() => handleAmount(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                </div>
+                <div className="right-section">{getFormattedPrice(price)}</div>
+              </div>
+            </li>
+          );
+        })}
+        <li>
+          <div className="toppings-list-item">
+            <div className="left-section">Total:</div>
+            <div className="right-section">{getFormattedPrice(total)}</div>
+          </div>
+        </li>
+      </ul>
         </label>
         <br />
         <label>
@@ -103,12 +169,6 @@ const errorMessage = () => {
         </label>
         <button type="button" onClick={handleAddInput}>Add</button>
         <br />
-        <ul>
-          {/* Displaying recent input pairs */}
-          {inputList.map((item, index) => (
-            <li key={index}>{`Name: ${item.name}, Number: ${item.number}`}</li>
-          ))}
-        </ul>
 
         <h2>All Input Pairs:</h2>
         <ul>
